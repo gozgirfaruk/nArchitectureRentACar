8eaf472b28c5f91b328b8380ce3148c1521c4cb3
gozgirfaruk <gozgirfaruk@gmail.com>
2024-10-22 15:27:51 +0300

    CQRS ve MediatR implementasyonu gerçekleştirildi.

---
+++ rentACar/Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Brands.Commands.Create
+{
+    public class CreateBrandCommand : IRequest<CreateBrandResponse>
+    {
+        public string Name { get; set; }
+
+        public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreateBrandResponse>
+        {
+            public Task<CreateBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+            {
+                CreateBrandResponse response = new CreateBrandResponse();
+                response.Name = request.Name;
+                response.Id = new Guid();
+                return null;
+            }
+        }
+    }
+}
---
+++ rentACar/RentACarApi/Controllers/BrandsController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.Brands.Commands.Create;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RentACarApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public BrandsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddBrand(CreateBrandCommand createBrand)
+        {
+           await _mediator.Send(createBrand);
+            return Ok("Başarılı bir şekilde marka eklendi");
+        }
+    }
+}
